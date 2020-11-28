50499398a6edaf9baa06e5d7e0f4f1a7868b44ca
Dan Tarleton <danieltarleton@hotmail.co.uk>
2020-11-28 11:29:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+#!flask/bin/python
+from flask import Flask
+from flask import render_template
+import pyautogui
+import pydirectinput
+import json
+import os
+app = Flask(__name__)
+
+@app.route('/buzz/')
+def main():
+  return render_template("index.html")
+
+@app.route('/buzz/buttons/<int:player>')
+def buttons(player):
+  with open('button_mappings/player_' + str(player) + '.json') as f:
+    player_mappings = json.load(f)
+  return render_template("buttons.html", player_mappings=player_mappings, server_url=os.environ.get("WEB_SERVER_URL"))
+
+@app.route('/buzz/trigger/<string:key>')
+def trigger(key):
+  pydirectinput.press(key)
+  return ""
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
